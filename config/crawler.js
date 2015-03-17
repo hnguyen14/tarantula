b8e82dec4c3ced0a0704cc53f8745d43aa8faaaf
Hoang Nguyen <hoang.nguyen.11084@gmail.com>
2015-03-17 01:23:25 -0700

    more error check

--- config/crawler.js
+++ config/crawler.js
@@ -5,12 +5,14 @@ var riak = require('./riak');
 
 var crawler = new Crawler({
   callback: function(error, htmlData) {
-    if (htmlData.url) {
+    if (htmlData && htmlData.url) {
       console.log('crawled', htmlData.url);
       riak.bucket('pages').objects
         .new(htmlData.url, htmlData)
         .save();
-      htmlData.links.forEach(dispatcher.dispatch);
+      htmlData.links.forEach(function(url) {
+        dispatcher.dispatch(url);
+      });
     }
   }
 });
